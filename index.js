e6345acf79f8fd398e813db64abef3459d8dc64a
Asmita-Biswas200 <asmita.biswas@bimaplan.co>
2024-06-14 12:32:30 +0530

    Changes in server.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const path = require('path')
 const app = express()
-const PORT = 3000;
+require('dotenv').config();
+
+const PORT = process.env.PORT || 8000;
 const server = app.listen(PORT, () => console.log(`Server on port ${PORT}`))
 
 const io = require('socket.io')(server)
